cfdeee8ad8e3654b890301b8cd51f420667b9c96
Peter Donka <peter.donka@gmail.com>
2023-08-30 11:28:06 +0200

    Update

--- build/start.sh
+++ build/start.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
 
 echo -n "Starting rsyslog..."
-systemctl start rsyslog
-echo "Done."
+systemctl start rsyslog > /dev/null 2>&1
+if [ $? -eq 0 ] || [ $? -eq 1 ]; then
+    echo OK
+else
+    echo FAIL
+    exit 1
+fi
+
+echo -n "Starting MariaDB..."
+systemctl start mariadb > /dev/null 2>&1
+if [ $? -eq 0 ] || [ $? -eq 1 ]; then
+    echo OK
+else
+    echo FAIL
+    exit 1
+fi
 
 if [ "$NO_START_ZNUNY" != "1" ]; then
-    echo -n "Starting MariaDB and httpd service..."
-    echo -n "mariadb..."
-    systemctl start mariadb
-    echo -n "httpd..."
-    systemctl start httpd
-    echo "Done."
+    echo -n "Starting Httpd service..."
+    systemctl start httpd > /dev/null 2>&1
+    if [ $? -eq 0 ] || [ $? -eq 1 ]; then
+	echo OK
+    else
+	echo FAIL
+	exit 1
+    fi
+
+    echo "Fixing Znuny permissions."
+    /opt/znuny/bin/otrs.SetPermissions.pl
     echo "Starting Znuny cron:"
     su -c '~/bin/Cron.sh start' znuny
     echo "Starting Znuny daemon:"
