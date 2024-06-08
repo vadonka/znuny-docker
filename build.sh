2bd57c19b383e4ff3aafee306db84de04654f62a
vadonka <peter.donka@gmail.com>
2024-06-08 20:01:59 +0200

    Update version to 7.0.18

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-
-docker pull almalinux:8.8
-docker build -t archanfelhun/znuny:7.0.10-01 ./build
-docker build -t archanfelhun/znuny:latest ./build
+docker build --pull -t archanfelhun/znuny:7.0.18-01 -t archanfelhun/znuny:latest ./build
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker push archanfelhun/znuny:7.0.9-01
+docker push archanfelhun/znuny:7.0.18-01
 docker push archanfelhun/znuny:latest
